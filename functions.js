49943e4bdddd4b67258612faaefe46dc1d0e26de
Joel Mendivil <jmendivil171@west-mec.org>
2021-03-26 15:25:06 -0700

    leet code solutions for 9, 34, and 412

--- functions.js
+++ functions.js
@@ -20,7 +20,7 @@
 // })
 
 
-//Leet Code Problem 66
+//Leet Code Problem 66 /////////////////////////////////////////////////////////////////////////
 let digits = [1,2,3];
 var plusOne = function(digits) {
     for(let i = digits.length - 1; i >= 0; i--){
@@ -35,7 +35,7 @@ var plusOne = function(digits) {
     return digits;
 };
 
-//Linear Search
+//Linear Search /////////////////////////////////////////////////////////////////////////
 const unsortedArr = [31, 27, 28, 42, 13, 8, 11, 30, 17, 41, 15, 43, 1, 36, 9, 16, 20, 35, 48, 37, 7, 26, 34, 21, 22, 6, 29, 32, 49, 10, 12, 19, 24, 38, 5, 14, 44, 40, 3, 50, 46, 25, 18, 33, 47, 4, 45, 39, 23, 2];
 const sortedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
 
@@ -51,7 +51,7 @@ const linear = (arr, target) => {
 console.log(linear(unsortedArr, 40)); //40 steps in 40 Milliseconds
 console.log(linear(sortedArr, 40)); //40 steps in 40 Milliseconds
 
-//Binary Search
+//Binary Search /////////////////////////////////////////////////////////////////////////
 const binary = (arr, target) => {
     let start = 0;
     let end = arr.length;
@@ -75,3 +75,60 @@ const binary = (arr, target) => {
 console.log(linear(unsortedArr, 40)); //Nothing Found
 console.log(binary(sortedArr, 44)); //2 steps in 7 Milliseconds
 console.log(binary(sortedArr, 43)); //5 steps in 8 Milliseconds
+
+//Leet Code Problem 9 /////////////////////////////////////////////////////////////////////////
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    return x == x.toString().split('').reverse().join('');
+};
+
+//Leet Code Problem 34 /////////////////////////////////////////////////////////////////////////
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    const length = nums.length - 1;
+    let start = 0;
+    let end = length;
+    let center;
+    
+    // get the center
+    while (start <= end) {
+        center = Math.floor((start + end) / 2);
+        if (nums[center] === target) {
+            start = center;
+            end = center;
+            break;
+        }
+        else if (nums[center] < target) start = center + 1;
+        else end = center - 1;
+    }
+
+    if (start > end) return [-1, -1];
+    
+    // find the edges
+    while (nums[start - 1] === target) start--;
+    while (nums[end + 1] === target) end++;
+    return [start, end];
+};
+
+//Leet Code Problem 412 /////////////////////////////////////////////////////////////////////////
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let result = [];
+    for (i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) result.push("FizzBuzz");
+        else if (i % 3 == 0) result.push("Fizz");
+        else if (i % 5 == 0) result.push("Buzz");
+        else result.push(i + "");
+    }
+    return result;
+};
