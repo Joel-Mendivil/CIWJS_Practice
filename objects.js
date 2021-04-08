c31b63d98f987d3003fd555bfbb5437a4e7b5425
Joel Mendivil <jmendivil171@west-mec.org>
2021-04-08 15:26:29 -0700

    Animals.js created

--- objects.js
+++ objects.js
@@ -151,13 +151,48 @@
 // }
 // console.log(averageScore(Catheryn));
 
-function Student(name, id, letter, grade) {
-    this.name = name;
-    this.id = id;
-    this.letter = letter;
-    this.grade = grade;
-    this.admin = false;
-}
+// function Student(name, id, letterGrade, gradeLv) {
+//     this.name = name;
+//     this.id = id;
+//     this.letterGrade = letterGrade;
+//     this.gradeLv = gradeLv;
+//     this.admin = false;
 
-let Joel = new Student('Joel Mendivil', 74932, 'A', 12);
-console.log(Joel);
+//     this.graduate = function() {
+//         return this.gradeLv++;
+//     }
+//     this.whiteList = function () {
+//         this.admin = true;
+//     }
+//     this.setGrade = function(value) {
+//         this.letterGrade = value;
+//     }
+// }
+
+// let Joel = new Student('Joel Mendivil', 74932, 'A', 12);
+// console.log(Joel);
+
+class Rectangle {
+    constructor(width, length) {
+        this.width = width;
+        this.length = length;
+    }
+    get area() {
+        return this.length * this.width;
+    }
+}
+const rect1 = new Rectangle(10, 5);
+const rect2 = new Rectangle(6, 12);
+const rect3 = new Rectangle(15, 20);
+
+//Example of how to access getter method
+const area = [rect1.area, rect2.area, rect3.area];
+
+class Circle {
+    set radius(radius) {
+        this._radius = radius;
+        this.area = 3.14 * Math.pow(radius, 2);
+        this.circumference = 2 * (3.14) * radius;
+    }
+}
+const circ = new Circle();   
